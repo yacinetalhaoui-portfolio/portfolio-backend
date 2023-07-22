70febd105be8e99fef58c3328f20a5d78faad704
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-22 03:46:47 +0200

    :wrench: railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ExperienceModule } from './experience/experience.module';
 import { EducationModule } from './education/education.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.DB_URI),
+    MongooseModule.forRoot(
+      'mongodb+srv://ycn:5t2kFLtCT6F8Stey@myinformations.yexqxsp.mongodb.net/mydb',
+    ),
     ProjectModule,
     ExperienceModule,
     EducationModule,
