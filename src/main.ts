deda2e2dcc85cc57661b7b8eb5332c0ee1a000be
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-23 14:20:50 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ const cors = require("cors");
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cors());
+  app.use(cors({
+    origin: "http://localhost:5173/",
+  }));
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
