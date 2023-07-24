bf742f0e44fd3d0eb72ac9d8241cf239687f6b0c
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-24 18:45:34 +0200

    🐛Ajout du tag api "EducationExperience" pour le swagger

--- src/education/education.controller.ts
+++ src/education/education.controller.ts
@@ -2,8 +2,10 @@ import {Body, Controller, Get, Post} from '@nestjs/common';
 import { EducationService } from './education.service';
 import { Education } from './schemas/education.schema';
 import {EducationDto} from "./dtos/education.dto";
+import {ApiTags} from "@nestjs/swagger";
 
 @Controller('education')
+@ApiTags("EducationExperience")
 export class EducationController {
   constructor(private readonly educationService: EducationService) {}
 
